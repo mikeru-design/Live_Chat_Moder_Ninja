6c782539222c2f24644d2ff3b9eddbc4522d3779
miekru <sajna.michal@gmail.com>
2023-03-24 14:31:01 +0100

    HTML & CSS prepared // Add message & live listener funtion done

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import { initializeApp } from 'firebase/app';
+import Chatroom from './js/chat';
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsX0I5_3l5scw6qXZ-rxA6RVrwCruHK4g",
+  authDomain: "modern-ninja-live-chat.firebaseapp.com",
+  projectId: "modern-ninja-live-chat",
+  storageBucket: "modern-ninja-live-chat.appspot.com",
+  messagingSenderId: "1002185451830",
+  appId: "1:1002185451830:web:3fe26e211764666ab0ab97",
+  measurementId: "G-9RM09PXBYD"
+};
+
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
+export const chatsColRef = collection(db, 'chats');
+
+onSnapshot(chatsColRef, (snapshot) => {
+  let chats = [];
+    for (const doc of snapshot.docs) {
+      chats.push({ ...doc.data(), id: doc.id })
+    }
+    console.log(snapshot.docs);
+    console.log(chats);
+})
+
+const chatroom = new Chatroom('general', 'mikeru');
+
+// chatroom.addChat('hello again')
+//   .then(() => console.log('chat added'))
+//   .catch(err => console.log(err));
+
---
+++ src/js/chat.js
@@ -0,0 +1,23 @@
+import { chatsColRef } from '..';
+import { addDoc } from 'firebase/firestore';
+
+export default class Chatroom {
+  constructor(room, username){
+    this.room = room;
+    this.username = username;
+    this.chats = chatsColRef;
+  }
+
+  async addChat(message) {
+    const chat = {
+      message,
+      username: this.username,
+      room: this.room,
+      created_at: new Date(),
+    };
+
+    const response = await addDoc(chatsColRef, chat);
+    return response;
+  }
+}
+
